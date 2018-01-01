f53003f33bd4076cda900cd8887daec7afdb9028
Yuki <yuki08281993@gmail.com>
2018-01-01 17:40:44 +0900

    modify the structure of directory tree

--- abc77/fetch.sh
+++
@@ -1,8 +0,0 @@
-cd A
-ruby ../../atcoder.rb abc77_tokinaoshi A
-cd ../B
-ruby ../../atcoder.rb abc77_tokinaoshi B
-cd ../C
-ruby ../../atcoder.rb abc77_tokinaoshi C
-cd ../D
-ruby ../../atcoder.rb abc77_tokinaoshi D

54257db5bb72278a11785a0f70aafb73af00f57f
Yuki <yuki08281993@gmail.com>
2018-07-28 21:21:03 +0900

    hoge

--- typescript/main.ts
+++ typescript/main.ts
@@ -1,2 +1,13 @@
-const hoge: string = 'Hcello!';
-console.log(hoge);
+declare function require(x: string): any;
+
+    const main = (input: string[][]) => {
+        const s: string[] = input[0];
+        if (s.every((c) => c != 'Y')) {
+            console.log('three');
+        } else {
+            console.log('four');
+        }
+    };
+
+const whole_input: string = require('fs').readFileSync('/dev/stdin', 'utf8');
+main(whole_input.split('\n').map((s: string) => s.split(' ')));
